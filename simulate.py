a6154e04c569db74bce86176dbba3051d239c3ba
bagineer <bih198721@gmail.com>
2022-12-04 02:45:50 +0900

    Feat: Add codes for simulating

--- simulate.py
+++ simulate.py
@@ -3,3 +3,56 @@ from classes import *
 if __name__ == '__main__':
     bank = Bank()
     atm = ATM(bank)
+
+    f = open('./testcases.txt', 'r')
+    T = int(f.readline())
+
+    for t in range(T):
+        print(f'[Test case #{t+1}]')
+        N = int(f.readline())
+
+        for n in range(N):
+            name, M = f.readline().split()
+            client = Client(name)
+            bank.add_client(client)
+
+            for m in range(int(M)):
+                account_number, account_password, balance, K = f.readline().split()
+                account = Account(name, account_number, account_password, int(balance))
+                add_account(client, account)
+                add_account(bank, account)
+
+                for k in range(int(K)):
+                    card_number, pin = f.readline().split()
+                    card = Card(card_number, account_number, pin)
+                    add_card(account, card)
+                    add_card(client, card)
+                    add_card(bank, card)
+
+        O = int(f.readline())
+
+        for o in range(O):
+            print('='*30)
+            print('Please insert your card.')
+            input_card_number = f.readline().strip()
+            print('- input card number :', input_card_number)
+            print()
+            searched_card = atm.read_card(input_card_number)
+            
+            print('Please enter pin number of the card.')
+            input_pin = f.readline().strip()
+            print('- input pin :', input_pin)
+            print()
+            is_correct = atm.verify_pin(input_pin)
+
+            if is_correct:
+                option = f.readline().split()
+                print('- selected option :', option)
+                print()
+                atm.select_option(*option)
+            atm.quit()
+            
+            print('='*30)
+            print()
+            print()
+    f.close()
